0a88420a7027ca0a5f5c54b770cb1bce783d0292
IngeCesar <cooh1@ymail.com>
2025-06-09 15:14:39 -0600

    Se creo la calse desafio y sus variables nombre, numero de cuenta y el saldo, como datos de la cuenta del usuario

---
+++ src/Desafio.java
@@ -0,0 +1,13 @@
+public class Desafio {
+
+    public static void main(String[] args) {
+        String nombre = "Tony Stark";
+        String tipoDeCuenta = "Corriente";
+        Double saldo = 1599.99;
+
+        System.out.println("**************");
+        System.out.println("\nNombre del cliente: " + nombre);
+        System.out.println("El tipo de cuenta es: " + tipoDeCuenta);
+        System.out.println("Su saldo disponible es: " + "L. " + saldo);
+    }
+}
