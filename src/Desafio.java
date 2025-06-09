a7dbf908996fa252ee566df4dc5d2c9d1c5edaab
IngeCesar <cooh1@ymail.com>
2025-06-09 15:35:46 -0600

    Se cro una salida que actua de menu para el usuario donde se da a elegir poder consultar el saldo, retirar o depositar y la opcion de salir del menu

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 public class Desafio {
 
     public static void main(String[] args) {
         String nombre = "Tony Stark";
         String tipoDeCuenta = "Corriente";
-        Double saldo = 1599.99;
+        double saldo = 1599.99;
+        int opcion =  0;
 
-        System.out.println("**************");
+        System.out.println("****************************************");
         System.out.println("\nNombre del cliente: " + nombre);
         System.out.println("El tipo de cuenta es: " + tipoDeCuenta);
         System.out.println("Su saldo disponible es: " + "L. " + saldo);
+        System.out.println("\n****************************************");
+        
+        String menu = """
+                *** Escriba el número de la opcion deseada ***
+                1 - Consultar saldo
+                2 - Retirar
+                3 - Depositar
+                9 - Salir
+                """;
+        Scanner teclado = new Scanner(System.in);
+        while (opcion != 9){
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+        }
     }
 }
